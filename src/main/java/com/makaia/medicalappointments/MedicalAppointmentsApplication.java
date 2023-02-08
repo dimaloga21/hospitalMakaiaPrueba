250f5902ca075abf36aede23f4e254e44a4333f4
dimaloga <dianiloga@gmail.com>
2023-02-08 13:53:23 -0500

    Creación de operación para creación, consulta, actualización y eliminación de citas médicas del hospital MAKAIA

---
+++ src/main/java/com/makaia/medicalappointments/MedicalAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.makaia.medicalappointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makaia/medicalappointments/MedicalAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makaia.medicalappointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
